083f5a069b0ea58bf905f05386c3fb1bc510bc9c
Thiago Adorno <thiago.adorno@aceflex.com.br>
2024-09-24 18:05:11 -0300

    Including asynchronicity in the get action

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,16 +1,15 @@
 ﻿using Blog.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        [HttpGet("categories")]
-        public IActionResult Get(
+        [HttpGet("v1/categories")]
+        public async Task<IActionResult> GetAsync(
             [FromServices] BlogDataContext context)
-                => Ok(context.Categories.ToList());
-
-
+                => Ok(await context.Categories.ToListAsync());
     }
 }

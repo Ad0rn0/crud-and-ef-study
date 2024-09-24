db924582414a99697bf075c2d6d407ae2917c60e
Thiago Adorno <thiago.adorno@aceflex.com.br>
2024-09-24 17:56:33 -0300

    #F0416: Async and Await

---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,23 @@
+using Blog.Data.Mappings;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=Blog;User ID=sa;Password=123");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+        }
+    }
+}

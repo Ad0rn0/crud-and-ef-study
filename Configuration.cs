51e228244f43f42c22fbf5563f9759c04ab3758c
Ad0rn0 <ad0rn0code@gmail.com>
2024-10-03 21:23:15 -0300

    #F0444: API Key

--- Configuration.cs
+++ Configuration.cs
@@ -3,5 +3,8 @@
 public static class Configuration
 {
     // TOKEN - JWT = Json Web Token
-    public static string JwtKey { get; set; } = "ZRQ3IazEV0KGlNTwh0wJYQ==";
+    public static string JwtKey = "ZRQ3IazEV0KGlNTwh0wJYQ==";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+
 }

ce865aeffe683c631964628c86219c8905164fce
Ad0rn0 <ad0rn0code@gmail.com>
2024-11-08 19:52:43 -0300

    #F0459: Added cache in get action categories

--- Program.cs
+++ Program.cs
@@ -52,6 +52,8 @@ void ConfigureAuthentication(WebApplicationBuilder builder)
 
 void ConfigureMvc(WebApplicationBuilder builder)
 {
+    builder.Services.AddMemoryCache();
+    
     builder
         .Services
         .AddControllers()
